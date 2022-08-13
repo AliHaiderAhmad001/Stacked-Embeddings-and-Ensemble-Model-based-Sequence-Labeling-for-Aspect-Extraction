83faf064cf7bbd51cb4be58c9d545bc43ff55ca6
AliHaiderAhmad001 <ali.h.ahmad001@gmail.com>
2022-08-13 17:56:05 +0300

    update

--- embeddings/BERT_ELMO_emb-20220811T130136Z-001/elmo_features.py
+++
@@ -1,8 +0,0 @@
-import tensorflow_hub as hub
-import tensorflow as tf
-class ELMoModelFeatures():
-    def __init__(self):
-      self.model = hub.load("https://tfhub.dev/google/elmo/3")
-    def elmo_vectors(self,sentence):
-      embeddings = self.model.signatures["default"](tf.constant(sentence))["elmo"]
-      return  tf.squeeze(embeddings)
